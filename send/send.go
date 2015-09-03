f2d2d679bda2477d4dd8e438cdb24dc637d949aa
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-03 16:06:04 +0200

    Polish send message, read line by line stdin.

--- send/send.go
+++ send/send.go
@@ -1,9 +1,12 @@
 package send
 
 import (
-	"fmt"
-	"log"
-	"rabbit/core"
+	"bufio"
+	"coelho/core"
+	"os"
+	"strings"
+
+	log "github.com/Sirupsen/logrus"
 
 	"github.com/streadway/amqp"
 )
@@ -11,25 +14,52 @@ import (
 func failOnError(err error, msg string) {
 	if err != nil {
 		log.Fatalf("%s: %s", msg, err)
-		panic(fmt.Sprintf("%s: %s", msg, err))
 	}
 }
 
-func Msg(body string) {
-	queue, err := core.Connect()
+/*
+ * TODO
+ *- write a goroutine which keeps the connection up and working all the time
+ *- buffered channel to send to it, so if the connection is down, then you can't send
+ */
+func Msg(r core.Rabbit, body string, rk string) {
+	err := r.Ch.Publish(
+		"events", // exchange
+		rk,       // routing key
+		false,    // mandatory
+		false,    // immediate
+		amqp.Publishing{
+			ContentType: "application/json",
+			Body:        []byte(body),
+		})
+	failOnError(err, "Failed to publish a message")
+}
+
+func BodyFromStdIn() {
+	queue := core.Rabbit{}
+	err := queue.Connect()
 	failOnError(err, "Failed to declare a queue")
 	defer queue.Con.Close()
 	defer queue.Ch.Close()
-	err = queue.Ch.Publish(
-		"",         // exchange
-		queue.Name, // routing key
-		false,      // mandatory
-		false,      // immediate
-		amqp.Publishing{
-			DeliveryMode: amqp.Persistent,
-			ContentType:  "text/plain",
-			Body:         []byte(body),
-		})
-	log.Printf(" [x] Sent %s", body)
-	failOnError(err, "Failed to publish a message")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		text := strings.Split(scanner.Text(), " ")
+		if len(text) > 1 {
+			Msg(queue, body(text), rk(text))
+		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func body(args []string) string {
+	s := strings.Join(args[1:], " ")
+	return s
+}
+
+func rk(args []string) string {
+	s := args[0]
+	return s
 }
