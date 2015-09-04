65da85fd8bf1f0939f30b3c3d3bf43a37324f31b
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-04 16:17:40 +0200

    move to gb

---
+++ set_env.sh
@@ -0,0 +1,13 @@
+#! /bin/sh
+#
+# set_env.sh
+# Copyright (C) 2015 giulio <giulioungaretti@me.com>
+#
+# Distributed under terms of the MIT license.
+#
+
+export rabbitMqAddres='192.168.56.10'
+export rabbitMqPort='5672'
+export rabbitMqUsr='guest'
+export rabbitMqPwd='guest'
+
