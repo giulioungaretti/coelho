4d52839ed4317a549f6674d340c3531e761aa07a
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-04 16:11:14 +0200

    Generalize core.
    Move sensible stuff into env. The rest of paraemters are commandline
    flags, whith sane un-harmfuls defaults.

--- core/main.go
+++ core/main.go
@@ -1,11 +1,41 @@
 package core
 
 import (
+	"flag"
 	"fmt"
+	"os"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/streadway/amqp"
 )
+
+var (
+	rabbitMqAddres string
+	rabbitMqPort   string
+	rabbitMqUsr    string
+	rabbitMqPwd    string
+)
+
+func init() {
+	// load env variables
+	// crahses if they don't exist
+	rabbitMqAddres = os.Getenv("rabbitMqAddres")
+	if rabbitMqAddres == "" {
+		log.Fatalf("RabbitMQ cluster addres was not found in the enviroment")
+	}
+	rabbitMqPort = os.Getenv("rabbitMqPort")
+	if rabbitMqPort == "" {
+		log.Fatalf("RabbitMQ cluster port was not found in the enviroment")
+	}
+	rabbitMqPwd = os.Getenv("rabbitMqPwd")
+	if rabbitMqPwd == "" {
+		log.Fatalf("RabbitMQ cluster pwd was not found in the enviroment")
+	}
+	rabbitMqUsr = os.Getenv("rabbitMqUsr")
+	if rabbitMqUsr == "" {
+		log.Fatalf("RabbitMQ cluster usr was not found in the enviroment")
+	}
+}
 
 func failOnError(err error, msg string) {
 	if err != nil {
@@ -14,60 +44,62 @@ func failOnError(err error, msg string) {
 }
 
 // RabbitMQConnection holds the details of a connection
-type RabbitMQConnection struct {
+type rabbitMQConnection struct {
 	addr string
 	port string
 }
 
 // format returns the formattet string connection to dial
-func (r *RabbitMQConnection) format() (s string) {
+func (r *rabbitMQConnection) format() (s string) {
 	s = fmt.Sprintf("amqp://guest:guest@%s:%s/", r.addr, r.port)
 	return
 }
 
 // Rabbit hold the details and the Con, Ch, Queue
 type Rabbit struct {
-	Con       *amqp.Connection
-	Ch        *amqp.Channel // channel to which the queue is attached
-	Queue     *amqp.Queue
-	Name      string
-	Durable   bool
-	Delete    bool // delete when usused
-	Exclusive bool
-	NoWait    bool
-	RK        string
-	Exchange  string
 	Arguments map[string]interface{}
+	Ch        *amqp.Channel // channel to which the queue is attached
+	Con       *amqp.Connection
+	Delete    bool // delete when usused
+	Durable   bool
+	Exchange  string
+	Exclusive bool
+	Name      string
+	NoWait    bool
+	Queue     *amqp.Queue
+	RK        string
+	Type      string
 }
 
-func (q *Rabbit) declareExc() {
+//DeclareExc decleares an exchange with false auto-delede, and false internal flags.
+func (q *Rabbit) DeclareExc() {
 	err := q.Ch.ExchangeDeclare(
-		q.Exchange, // name
-		"topic",    // type
-		true,       // durable
-		false,      // auto-deleted
-		false,      // internal
-		false,      // no-wait
-		nil,        // arguments
+		q.Exchange,  // name
+		q.Type,      // type
+		q.Durable,   // durable
+		false,       // auto-deleted
+		false,       // internal
+		q.NoWait,    // no-wait
+		q.Arguments, // arguments
 	)
 	failOnError(err, "Failed to declare an exchange")
 }
 
-// bind the queue to an exchange
-func (r *Rabbit) bind() {
+// Bind the queue to an exchange
+func (r *Rabbit) Bind() {
 	log.Printf("Binding queue %s to exchange %s with routing key %s",
 		r.Queue.Name, r.Exchange, r.RK)
 	err := r.Ch.QueueBind(
-		r.Queue.Name, // queue name
-		r.RK,         // routing key
-		r.Exchange,   // exchange
-		false,
-		nil)
+		r.Queue.Name,
+		r.RK,
+		r.Exchange,
+		r.NoWait,
+		r.Arguments)
 	failOnError(err, "Failed to bind a queue")
 }
 
-// decleare returns a decleared queue
-func (q *Rabbit) declareQueue() {
+// DeclareQueue returns a decleared queue
+func (q *Rabbit) DeclareQueue() {
 	qd, err := q.Ch.QueueDeclare(
 		q.Name,
 		q.Durable,
@@ -80,10 +112,20 @@ func (q *Rabbit) declareQueue() {
 	failOnError(err, "Failed to declare a queue")
 }
 
-// Connect starts an amqp connection and returns an initialized queue.
-// TODO this should read from env, or json or something alike
-func (q *Rabbit) Connect() error {
-	c := RabbitMQConnection{}
+// ConnectRaw connects a rabbit. The rabbit must know what to do.  So one has
+// to manually  intialize the struct.  The rabbit will try to connect, create
+// channel, exchange, queue and bind it to the exchange
+//Example:
+//q.Name = "events"
+//q.RK = "#"
+//// mimimal guarantee to not lose messages.
+//q.Durable = true
+//q.Delete = false
+//q.Exclusive = false
+//q.NoWait = false
+//q.Arguments = nil
+func (q *Rabbit) ConnectRaw() error {
+	c := rabbitMQConnection{}
 	c.addr = "192.168.56.10"
 	c.port = "5672"
 	conn, err := amqp.Dial(c.format())
@@ -92,17 +134,49 @@ func (q *Rabbit) Connect() error {
 	failOnError(err, "Failed to open a channel")
 	q.Con = conn
 	q.Ch = ch
-	q.Name = "events"
-	q.Exchange = "events"
-	q.RK = "#"
-	// mimimal guarantee to not lose messages.
-	q.Durable = true
-	q.Delete = false
-	q.Exclusive = false
-	q.NoWait = false
+	q.DeclareExc()
+	q.DeclareQueue()
+	q.Bind()
+	return err
+}
+
+// ConnectRaw connects a Rabbit. The rabbit know what to do based on flags.
+func (q *Rabbit) Connect() error {
+	// init empty rabbit
+	c := rabbitMQConnection{}
+	c.port = rabbitMqPort
+	c.addr = rabbitMqAddres
+	conn, err := amqp.Dial(c.format())
+	failOnError(err, "Failed to connect to RabbitMQ")
+	ch, err := conn.Channel()
+	failOnError(err, "Failed to open a channel")
+
+	// parse flags
+	exch := flag.String("exchange", "events", "name of the rabbit exchange")
+	name := flag.String("name", "events", "name of rabbit queue")
+	rk := flag.String("rk", "#", "routing key to bind to")
+	durable := flag.Bool("durable", true, "make the queue durable")
+	nowait := flag.Bool("nowait", false, "make the queue nowait")
+	delete := flag.Bool("delete", false, "make the queue delete")
+	exclusive := flag.Bool("exclusive", false, "make the queue exclusive")
+	flag.Parse()
+
+	// now update the rabbitt
+	q.Con = conn
+	q.Ch = ch
+	q.Exchange = *exch
+	q.Name = *name
+	q.RK = *rk
+	q.Durable = *durable
+	q.Delete = *delete
+	q.Exclusive = *exclusive
+	q.NoWait = *nowait
+	// TODO  nil for now, we won't need them anyway.
 	q.Arguments = nil
-	q.declareExc()
-	q.declareQueue()
-	q.bind()
+
+	// exc-queue-bind
+	q.DeclareExc()
+	q.DeclareQueue()
+	q.Bind()
 	return err
 }
