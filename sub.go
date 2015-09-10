ad30079168e5f4bcb0f2f0cb66c87be2df6836a1
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-10 15:59:58 +0200

    Cleanup.

--- sub.go
+++ sub.go
@@ -5,46 +5,40 @@ import (
 	"golang.org/x/net/context"
 )
 
-// subscribe consumes deliveries from an exclusive queue from a fanout exchange and sends to the application specific messages chan.
+// Subscribe consumes deliveries from an exclusive queue from an exchange and sends to the application specific messages chan.
+// handles shutting down gracefully in case of sig-int. Or disconnects.
 func (r Rabbit) Subscribe(sessions chan Session, messages chan<- Message, done context.CancelFunc) {
 	DieGracefully(done)
 	queue := r.Name
 	// subscribe forever
 	for {
 		for sub := range sessions {
-			//sub := <-Session
 			// declaere quque
 			if _, err := r.DeclareQueue(sub.Channel); err != nil {
-				log.Printf("cannot consume from exclusive queue: %q, %v", queue, err)
-				return
+				log.Errorf("cannot consume from exclusive queue: %q, %v", queue, err)
+				// try again
+				continue
 			}
 
 			if err := r.Bind(sub.Channel); err != nil {
-				log.Printf("cannot consume without a binding to exchange: %+v, %v", r, err)
-				return
+				log.Errorf("cannot consume without a binding to exchange: %+v, %v", r, err)
+				continue
 			}
 			autoAck := true
 			nowait := true
 			deliveries, err := sub.Consume(queue, "", autoAck, false, false, nowait, nil)
 			if err != nil {
-				log.Printf("cannot consume from: %q, %v", queue, err)
-				return
+				log.Errorf("cannot consume from: %q, %v", queue, err)
+				// try again
+				continue
 			}
-			log.Printf("subscribed...")
-			// connection close
-			// channel down
-			// sub closed
-			i := 0
 			for msg := range deliveries {
-				i++
-				// test breaking, and thus restarting
-				if a := i % 100; a == 0 {
-					messages <- Message(msg.Body)
-					log.Printf("breaking")
-					break
-				}
+				messages <- Message(msg.Body)
 			}
+			log.Infof("Closed session.")
 			sub.Close()
+			// try again
+			continue
 		}
 	}
 }
