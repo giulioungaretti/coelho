72bbe0f4bd1b699db0bdb03662e6ab36f6badda8
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-10 15:10:50 +0200

    Change to auto-ack.

--- sub.go
+++ sub.go
@@ -23,23 +23,26 @@ func (r Rabbit) Subscribe(sessions chan Session, messages chan<- Message, done c
 				log.Printf("cannot consume without a binding to exchange: %+v, %v", r, err)
 				return
 			}
-
-			deliveries, err := sub.Consume(queue, "", false, false, false, true, nil)
+			autoAck := true
+			nowait := true
+			deliveries, err := sub.Consume(queue, "", autoAck, false, false, nowait, nil)
 			if err != nil {
 				log.Printf("cannot consume from: %q, %v", queue, err)
 				return
 			}
 			log.Printf("subscribed...")
+			// connection close
+			// channel down
+			// sub closed
 			i := 0
 			for msg := range deliveries {
 				i++
 				// test breaking, and thus restarting
 				if a := i % 100; a == 0 {
 					messages <- Message(msg.Body)
-					//log.Printf("breaking")
-					//break
+					log.Printf("breaking")
+					break
 				}
-				sub.Ack(msg.DeliveryTag, false)
 			}
 			sub.Close()
 		}
