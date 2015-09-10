1b5b10aef9168e6a7ef2877e691cb8ec792d8c54
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-10 15:09:42 +0200

    Add subscribe forever.

--- sub.go
+++ sub.go
@@ -6,33 +6,42 @@ import (
 )
 
 // subscribe consumes deliveries from an exclusive queue from a fanout exchange and sends to the application specific messages chan.
-func (r Rabbit) Subscribe(sessions chan chan Session, messages chan<- Message, done context.CancelFunc) {
+func (r Rabbit) Subscribe(sessions chan Session, messages chan<- Message, done context.CancelFunc) {
 	DieGracefully(done)
 	queue := r.Name
-	for Session := range sessions {
-		sub := <-Session
-		// declaere quque
-		if _, err := r.DeclareQueue(sub.Channel); err != nil {
-			log.Printf("cannot consume from exclusive queue: %q, %v", queue, err)
-			return
-		}
+	// subscribe forever
+	for {
+		for sub := range sessions {
+			//sub := <-Session
+			// declaere quque
+			if _, err := r.DeclareQueue(sub.Channel); err != nil {
+				log.Printf("cannot consume from exclusive queue: %q, %v", queue, err)
+				return
+			}
 
-		if err := r.Bind(sub.Channel); err != nil {
-			log.Printf("cannot consume without a binding to exchange: %+v, %v", r, err)
-			return
-		}
+			if err := r.Bind(sub.Channel); err != nil {
+				log.Printf("cannot consume without a binding to exchange: %+v, %v", r, err)
+				return
+			}
 
-		deliveries, err := sub.Consume(queue, "", false, true, false, false, nil)
-		if err != nil {
-			log.Printf("cannot consume from: %q, %v", queue, err)
-			return
-		}
-
-		log.Printf("subscribed...")
-
-		for msg := range deliveries {
-			messages <- Message(msg.Body)
-			sub.Ack(msg.DeliveryTag, false)
+			deliveries, err := sub.Consume(queue, "", false, false, false, true, nil)
+			if err != nil {
+				log.Printf("cannot consume from: %q, %v", queue, err)
+				return
+			}
+			log.Printf("subscribed...")
+			i := 0
+			for msg := range deliveries {
+				i++
+				// test breaking, and thus restarting
+				if a := i % 100; a == 0 {
+					messages <- Message(msg.Body)
+					//log.Printf("breaking")
+					//break
+				}
+				sub.Ack(msg.DeliveryTag, false)
+			}
+			sub.Close()
 		}
 	}
 }
