44feb8ababec47c4d12c288a0eaff5a49f6d74fc
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-10 16:02:51 +0200

    Polish and use bigger pending channel.

--- publish.go
+++ publish.go
@@ -10,17 +10,16 @@ import (
 
 // publish publishes messages to a reconnecting Session to a fanout exchange.
 // It receives from the application specific source of messages.
-func (r Rabbit) Publish(sessions chan chan Session, messages <-chan Message, done context.CancelFunc) {
+// Keeps a pending channel in case of small timeouts.
+func (r Rabbit) Publish(sessions chan Session, messages <-chan Message, done context.CancelFunc) {
 	defer DieGracefully(done)
 	var (
 		running bool
 		reading = messages
-		pending = make(chan Message, 1)
+		pending = make(chan Message, 10)
 	)
 
-	for Session := range sessions {
-		pub := <-Session
-		// declaere quque
+	for pub := range sessions {
 		if _, err := r.DeclareQueue(pub.Channel); err != nil {
 			log.Printf("cannot consume from exclusive queue: %q, %v", r.Name, err)
 			return
@@ -31,7 +30,6 @@ func (r Rabbit) Publish(sessions chan chan Session, messages <-chan Message, don
 			select {
 			default:
 				reading = messages
-				panic("FOO")
 			case body = <-pending:
 				//TODO pass info here
 				// exchange, key string, mandatory, immediate bool, msg Publishing
