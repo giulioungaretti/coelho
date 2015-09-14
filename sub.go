43ca14c71f7a31882b42da5ab68b06aebe16b9d2
Giulio Ungaretti <giulio@Giulios-MacBook-Pro.local>
2015-09-14 16:38:57 +0200

    Cleanup code.

--- sub.go
+++ sub.go
@@ -11,40 +11,37 @@ func (r Rabbit) Subscribe(sessions chan Session, messages chan<- Message, ctx co
 	DieGracefully(done)
 	queue := r.Name
 	// subscribe forever
-	for {
-		for sub := range sessions {
-			// declaere quque
-			if _, err := r.DeclareQueue(sub.Channel); err != nil {
-				log.Errorf("cannot consume from exclusive queue: %q, %v", queue, err)
-				// try again
-				continue
-			}
-
-			if err := r.Bind(sub.Channel); err != nil {
-				log.Errorf("cannot consume without a binding to exchange: %+v, %v", r, err)
-				continue
-			}
-			autoAck := true
-			nowait := true
-			deliveries, err := sub.Consume(queue, "", autoAck, false, false, nowait, nil)
-			if err != nil {
-				log.Errorf("cannot consume from: %q, %v", queue, err)
-				// try again
-				continue
-			}
-			for msg := range deliveries {
-				// handle done signaling
-				select {
-				case messages <- Message{msg.Body, msg.RoutingKey}:
-				case <-ctx.Done():
-					return
-				}
-			}
-			log.Infof("Closed session.")
-			sub.Close()
+	for sub := range sessions {
+		// declaere quque
+		if _, err := r.DeclareQueue(sub.Channel); err != nil {
+			log.Errorf("cannot consume from exclusive queue: %q, %v", queue, err)
 			// try again
 			continue
-			done()
+		}
+
+		if err := r.Bind(sub.Channel); err != nil {
+			log.Errorf("cannot consume without a binding to exchange: %+v, %v", r, err)
+			continue
+		}
+		autoAck := true
+		nowait := true
+		deliveries, err := sub.Consume(queue, "", autoAck, false, false, nowait, nil)
+		if err != nil {
+			log.Errorf("cannot consume from: %q, %v", queue, err)
+			// try again
+			continue
+		}
+		for msg := range deliveries {
+			//this will never end because deliveries is closed
+			// only on connection/amqp-channel errors.
+			select {
+			case messages <- Message{msg.Body, msg.RoutingKey}:
+			case <-ctx.Done():
+				log.Infof("Closed session.")
+				sub.Close()
+				done()
+				return
+			}
 		}
 	}
 }
