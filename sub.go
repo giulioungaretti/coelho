956d8d0f5356dd64e52539986dfc6528b4be4a5b
giulio <giulioungaretti@me.com>
2015-11-28 14:17:25 +0100

    Test  bunching on the driver.

--- sub.go
+++ sub.go
@@ -2,7 +2,6 @@ package coelho
 
 import (
 	"sync/atomic"
-	"time"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/streadway/amqp"
@@ -14,7 +13,7 @@ import (
 //******** NOTE that mesasges are not acked.**************
 // it's the client responsability  to ack the message.
 //Handles shutting down gracefully in case of sig-int. Or disconnects.
-func (r Rabbit) Subscribe(ctx context.Context, sessions chan Session, messages chan<- Message, queueName string, counts *uint64) {
+func (r Rabbit) Subscribe(ctx context.Context, sessions chan Session, messages chan []amqp.Delivery, queueName string, counts *uint64) {
 	// subscribe forever
 	for sub := range sessions {
 		// declaere quque
@@ -36,39 +35,30 @@ func (r Rabbit) Subscribe(ctx context.Context, sessions chan Session, messages c
 			// try again
 			continue
 		}
+		var buffer []amqp.Delivery
+		lenMsg := r.QoS
+		var i int
 		select {
 		default:
-			for msg := range deliveries {
-				//TODO  this can be used already to bunch up the results.
-				// sending a []amqp.delivery instead of a single deveivery
-
-				//this will never end because deliveries is closed
-				// only on connection/amqp-channel errors.
+		loop:
+			for {
 				select {
-				case <-time.After(1 * time.Second):
-					// if we wait more than 1 * Second to send thorough the
-					// channel it means  that the reciever is blocked so we just
-					// exit and avoid losing too much messages
-					log.Warnf("Timeout")
-					mutliple := true
-					requeue := true
-					msg.Nack(mutliple, requeue)
-					continue
-				// TODO WTF ? this strcut contains way too much redundant information
-				// you idiot
-				case messages <- Message{
-					Body: msg.Body,
-					Rk:   msg.RoutingKey,
-					Msg:  msg,
-				}:
-					// msg is not acked
+				case msg := <-deliveries:
+					buffer = append(buffer, msg)
 					atomic.AddUint64(counts, 1)
+					i++
 				case <-ctx.Done():
-					log.Infof("Closed session.")
-					sub.Close()
-					return
+					break loop
+				default:
+				}
+				if i == lenMsg {
+					messages <- buffer
+					buffer = make([]amqp.Delivery, 0)
+					i = 0
 				}
 			}
+			log.Infof("Closed session.")
+			sub.Close()
 		case <-ctx.Done():
 			log.Infof("Closed session.")
 			sub.Close()
