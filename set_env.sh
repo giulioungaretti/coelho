8332a25cd1d285f33dd866117b7e36934d3c204a
giulio <giulioungaretti@me.com>
2015-12-07 10:15:04 +0100

    Add all environment variables.

--- set_env.sh
+++ set_env.sh
@@ -12,3 +12,39 @@ pass='guest'
 vhost=''
 rabbitMqAddres='amqp://'$user':'$pass'@'$host':'$port'/'$vhost
 export rabbitMqAddres
+
+
+# exchange name
+export exchange_name="events"
+export exchange_type="direct"
+# if QoS = 0 it is disabled which is what we mostly want
+export QoS=0
+
+
+#local  rabbit mq clusta
+host='192.168.33.11'
+port='5672'
+user='guest'
+pass='guest'
+vhost=''
+local_rabbitMQ_addres='amqp://'$user':'$pass'@'$host':'$port'/'$vhost
+export local_rabbitMQ_addres
+# exchange name
+export local_exchange_name="events"
+export local_exchange_type="direct"
+# if QoS = 0 it is disabled which is what we mostly want
+export QoS=0
+
+
+
+
+
+
+
+
+
+
+
+
+
+
